ca155d72226ddc47b50bbf550a4083ad9b541616
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-08-22 21:25:26 +0600

    added js code

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var que = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < que.length; i++) {
+  que[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
